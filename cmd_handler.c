febab963a62cd527fa3e9e433e0e402ea7481e3a
GeissT <geisst-au@hotmail.com>
2012-01-29 15:52:37 +1030

    added GPL

--- cmd_handler.c
+++ cmd_handler.c
@@ -1,3 +1,23 @@
+/*  
+    --cmd_handler.c--
+    Copyright (C) 2012 GeissT
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    IMPLEMENTS THE COMMAND HANDLER
+*/
+
 #include "cmd_handler.h"
 #include <stdio.h>
 #include <stdlib.h>
